0d2a1024ee43492904118b74655593e604edc358
krzemu <ltryczynski1@gmail.com>
2025-03-31 15:48:36 +0200

    Added list to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/header";
+import ProductList from "./components/product-list";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <ProductList className="mt-6 container mx-auto" />
     </div>
   );
 }
