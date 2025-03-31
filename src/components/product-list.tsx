c993666ef2da03f94e47f1d379398b4dafc74cc9
krzemu <ltryczynski1@gmail.com>
2025-03-31 15:44:50 +0200

    Added product-list component

---
+++ src/components/product-list.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { DUMMY_PRODUCTS } from "../lib/data";
+import ProductCard from "./product-card";
+import { cn } from "../lib/utils";
+
+export default function ProductList({ className }: { className?: string }) {
+  return (
+    <section className={cn("grid grid-cols-4 gap-6", className)}>
+      {DUMMY_PRODUCTS.map((product) => (
+        <ProductCard product={product} />
+      ))}
+    </section>
+  );
+}
