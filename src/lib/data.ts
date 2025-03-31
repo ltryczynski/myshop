7b3c92317b86a408d195f5cbdcd2916ac5b062c7
krzemu <ltryczynski1@gmail.com>
2025-03-31 15:19:24 +0200

    added sample product data to lib/data.ts

---
+++ src/lib/data.ts
@@ -0,0 +1,73 @@
+
+export const DUMMY_PRODUCTS = [
+  {
+    id: 1,
+    title: "Product 1",
+    description: "Description for Product 1",
+    price: 10.0,
+    imageUrl: "https://images.pexels.com/photos/90946/pexels-photo-90946.jpeg",
+  },
+  {
+    id: 2,
+    title: "Product 2",
+    description: "Description for Product 2",
+    price: 20.0,
+    imageUrl: "https://images.pexels.com/photos/335257/pexels-photo-335257.jpeg",
+  },
+  {
+    id: 3,
+    title: "Product 3",
+    description: "Description for Product 3",
+    price: 30.0,
+    imageUrl: "https://images.pexels.com/photos/1667088/pexels-photo-1667088.jpeg",
+  },
+  {
+    id: 4,
+    title: "Product 4",
+    description: "Description for Product 4",
+    price: 40.0,
+    imageUrl: "https://images.pexels.com/photos/2536965/pexels-photo-2536965.jpeg",
+  },
+  {
+    id: 5,
+    title: "Product 5",
+    description: "Description for Product 5",
+    price: 50.0,
+    imageUrl: "https://images.pexels.com/photos/248412/pexels-photo-248412.jpeg",
+  },
+  {
+    id: 6,
+    title: "Product 6",
+    description: "Description for Product 6",
+    price: 60.0,
+    imageUrl: "https://images.pexels.com/photos/2587370/pexels-photo-2587370.jpeg",
+  },
+  {
+    id: 7,
+    title: "Product 7",
+    description: "Description for Product 7",
+    price: 70.0,
+    imageUrl: "https://images.pexels.com/photos/2533266/pexels-photo-2533266.jpeg",
+  },
+  {
+    id: 8,
+    title: "Product 8",
+    description: "Description for Product 8",
+    price: 80.0,
+    imageUrl: "https://images.pexels.com/photos/4158/apple-iphone-smartphone-desk.jpg",
+  },
+  {
+    id: 9,
+    title: "Product 9",
+    description: "Description for Product 9",
+    price: 90.0,
+    imageUrl: "https://images.pexels.com/photos/258244/pexels-photo-258244.jpeg",
+  },
+  {
+    id: 10,
+    title: "Product 10",
+    description: "Description for Product 10",
+    price: 100.0,
+    imageUrl: "https://images.pexels.com/photos/279906/pexels-photo-279906.jpeg",
+  }
+]
